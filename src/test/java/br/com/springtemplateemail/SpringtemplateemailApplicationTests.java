eeae44f50298a602f0c4b4eaa6c7a58a43ee0b11
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2023-01-07 23:27:03 -0300

    Teste

---
+++ src/test/java/br/com/springtemplateemail/SpringtemplateemailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springtemplateemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtemplateemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
