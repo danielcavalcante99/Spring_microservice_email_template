f5e46591a426e450c74ed0a41a719ed829ecb9f3
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2023-01-07 23:22:26 -0300

    Application

---
+++ src/main/java/br/com/springtemplateemail/SpringtemplateemailApplication.java
@@ -0,0 +1,13 @@
+package br.com.springtemplateemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtemplateemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtemplateemailApplication.class, args);
+	}
+
+}
